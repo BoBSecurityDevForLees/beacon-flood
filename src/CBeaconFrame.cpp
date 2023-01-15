8efb2fa20cf8e075de972b49dc149e46ba1964f8
arad4228 <arad4228@gmail.com>
2023-01-15 20:58:26 +0900

    [KyoungJae]Fix BeaconFram Class for refactoring

--- src/CBeaconFrame.cpp
+++ src/CBeaconFrame.cpp
@@ -3,42 +3,17 @@
 // flooding Initializer
 C80211BeaconFrame::C80211BeaconFrame()
 {
-    // Random Library 초기화
-    srand(time(NULL));
-
-    u_char byteffff[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    // DestinationAddress Setting 
-    memcpy(&this->strDestinationAddress, byteffff, 6);
-
-
-    u_char byteRandom[6] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
+    this->framecontrol = 0x0080;
+    this->strDuration = 0x0000;
+    u_char byteBroadCast[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+    memcpy(&this->strDestinationAddress, byteBroadCast, 6);
 
     // Source Address Setting
-    memcpy(&this->strSourceAddress, byteRandom, 6);
+    generateRandomByte(&this->strSourceAddress[0]);
 
     // BSSID Setting
     // Source Address to BSSID Copy 6Byte
     memcpy(&this->strBSSID, &this->strSourceAddress, 6);
-}
-
-// parsing Initializer
-C80211BeaconFrame::C80211BeaconFrame(const u_char* packet)
-{
-    u_char temp[6];
-    // DestinationAddress 복사
-    memcpy(&temp, &packet[4], 6);
-    sprintf(this->strDestinationAddress,"%02x:%02x:%02x:%02x:%02x:%02x", temp[0], temp[1], temp[2], temp[3], temp[4], 
-    temp[5]);
-
-    // SourceAddress 복사
-    memcpy(&temp, &packet[10], 6);
-    sprintf(this->strSourceAddress,"%02x:%02x:%02x:%02x:%02x:%02x", temp[0], temp[1], temp[2], temp[3], temp[4], 
-    temp[5]);
-
-    // BSSID 복사
-    memcpy(&temp, &packet[16], 6);
-    sprintf(this->strBSSID,"%02x:%02x:%02x:%02x:%02x:%02x", temp[0], temp[1], temp[2], temp[3], temp[4], 
-    temp[5]);
 }
 
 C80211BeaconFrame::~C80211BeaconFrame()
@@ -46,43 +21,31 @@ C80211BeaconFrame::~C80211BeaconFrame()
 
 }
 
-char* C80211BeaconFrame::getDestinationAddress()
+bool C80211BeaconFrame::generateRandomByte(u_char* strRandomByte)
 {
-    return this->strDestinationAddress;
-}
+    // Random Library 초기화
+    srand(time(NULL));
 
-char* C80211BeaconFrame::getSourceAddress()
-{
-    return this->strSourceAddress;
-}
-
-char* C80211BeaconFrame::getBSSID()
-{
-    return this->strBSSID;
+    sprintf((char*)strRandomByte, "%02x%02x%02x%02x%02x%02x", rand()%255, rand()%255,
+    rand()%255, rand()%255, rand()%255, rand()%255);
+    return true;
 }
 
 bool C80211BeaconFrame::getFloodBeaconFrame(char* packet)
 {
-    char packetBeacon[24] = {0x00, };
-    // Beacon Signal
-    packetBeacon[0] = 0x80;
-    packetBeacon[1] = 0x00;
-    
+    // Beacon framecontrol
+    memcpy(&packet[0], &this->framecontrol, 2);
     // Duration Time
-    memcpy(&packetBeacon[2], this->strDuration, 2);
+    memcpy(&packet[2], &this->strDuration, 2);
     
     // Destination, SourceAddress, BSSID
-    memcpy(&packetBeacon[4], this->strDestinationAddress, 6);
-    memcpy(&packetBeacon[10], this->strSourceAddress, 6);
-    memcpy(&packetBeacon[16], this->strBSSID, 6);
+    memcpy(&packet[4], this->strDestinationAddress, 6);
+    memcpy(&packet[10], this->strSourceAddress, 6);
+    memcpy(&packet[16], this->strBSSID, 6);
     
     // Fragment, sequence number
-    packetBeacon[22] = 0xC0;
-    packetBeacon[23] = 0x38;
-
-    // 내부 값에서 전달받은 포인터로 값 복사
-    memcpy(&packet[0], packetBeacon, 24);
-
+    packet[22] = 0xC0;
+    packet[23] = 0x38;
     return true;
 }
 
--- src/CBeaconFrame.h
+++ src/CBeaconFrame.h
@@ -7,19 +7,18 @@
 class C80211BeaconFrame
 {
 private:
-    u_char strDuration[2] = {0x00, 0x00};   
-    char strDestinationAddress[18];
-    char strSourceAddress[18];
-    char strBSSID[18];
+    uint16_t framecontrol;
+    uint16_t strDuration;
+    u_char strDestinationAddress[6];
+    u_char strSourceAddress[6];
+    u_char strBSSID[6];
+
+    bool generateRandomByte(u_char* strRandomByte);
 
 public:
-    C80211BeaconFrame();                        // Flooding을 위한 생성자
-    C80211BeaconFrame(const u_char* packet);    // Parsing을 위한 생성자
+    C80211BeaconFrame();
     ~C80211BeaconFrame();
 
-    char* getDestinationAddress();
-    char* getSourceAddress();
-    char* getBSSID();
     bool getFloodBeaconFrame(char* packet);
     int getFloodBeaconFrameSize();
 };
